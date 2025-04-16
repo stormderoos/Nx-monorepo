9f144eb4adb1dd12d456c5197e865f01c1e91e3f
stormderoos <sw.deroos@student.avans.nl>
2025-04-16 17:07:34 +0200

    neo4j weg

--- apps/data-api/webpack.config.js
+++ apps/data-api/webpack.config.js
@@ -1,12 +1,8 @@
 const { composePlugins, withNx } = require('@nx/webpack');
-const path = require('path');
 
+// Nx plugins for webpack.
 module.exports = composePlugins(withNx(), (config) => {
-  config.output = {
-    ...config.output,
-    filename: 'main.js',
-    path: path.resolve(__dirname, '../../dist/apps/data-api'),
-  };
-
+  // Update the webpack config as needed here.
+  // e.g. `config.plugins.push(new MyPlugin())`
   return config;
 });
