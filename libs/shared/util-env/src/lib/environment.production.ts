9c9b4a865140539162a57d68fc7de22c220a7d5f
stormderoos <sw.deroos@student.avans.nl>
2025-04-16 23:20:11 +0200

    environment correct

--- libs/shared/util-env/src/lib/environment.production.ts
+++ libs/shared/util-env/src/lib/environment.production.ts
@@ -3,8 +3,8 @@ import { IEnvironment } from './environment.interface';
 export const environment: IEnvironment = {
     production: true,
 
-    ROOT_DOMAIN_URL: 'http://localhost:3000',
-    dataApiUrl: 'http://localhost:3000/api',
+    ROOT_DOMAIN_URL: 'https://jolly-meadow-00d0ed103.5.azurestaticapps.net',
+    dataApiUrl: 'https://nx-worskhop-nestjs-acdcg6hqd9b4cja2.westeurope-01.azurewebsites.net/api',
 
     MONGO_DB_CONNECTION_STRING: 'mongodb+srv://swderoos:swWelkom01!@footballdbcluster.vsocs.mongodb.net/',
 };
